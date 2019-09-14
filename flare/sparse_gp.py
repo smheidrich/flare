ce1c037f25b9368a4d9f38be4f92ea82977c823c
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-09-14 19:13:15 -0400

    clean up sparse_gp.py

--- flare/sparse_gp.py
+++ flare/sparse_gp.py
@@ -1,3 +1,4 @@
+"""Sparse Gaussian process regression."""
 import numpy as np
 
 
@@ -14,8 +15,8 @@ class SparseGP:
         self.sparse_environments = []
         self.training_structures = []
 
-        self.K_mm = np.zeros(0)
-        self.K_nm = np.zeros(0)
+        self.k_mm = np.zeros(0)
+        self.k_nm = np.zeros(0)
         self.lam = np.zeros(0)
 
     def add_sparse_point(self, atomic_env):
@@ -27,31 +28,31 @@ class SparseGP:
         # update list of sparse environments
         self.sparse_environments.append(atomic_env)
 
-        # update K_mm
-        prev_mm_size = self.K_mm.shape[0]
-        K_mm_updated = np.zeros((prev_mm_size+1, prev_mm_size+1))
-        K_mm_updated[:prev_mm_size, :prev_mm_size] = self.K_mm
+        # update k_mm
+        prev_mm_size = self.k_mm.shape[0]
+        k_mm_updated = np.zeros((prev_mm_size+1, prev_mm_size+1))
+        k_mm_updated[:prev_mm_size, :prev_mm_size] = self.k_mm
 
         for count, sparse_env in enumerate(self.sparse_environments):
             energy_kern = self.environment_kernel(atomic_env, sparse_env)
-            K_mm_updated[prev_mm_size, count] = energy_kern
-            K_mm_updated[count, prev_mm_size] = energy_kern
+            k_mm_updated[prev_mm_size, count] = energy_kern
+            k_mm_updated[count, prev_mm_size] = energy_kern
 
-        self.K_mm = K_mm_updated
+        self.k_mm = k_mm_updated
 
-        # update K_nm
-        prev_nm_size = self.K_nm.shape
-        K_nm_updated = np.zeros((prev_nm_size[0], prev_nm_size[1] + 1))
+        # update k_nm
+        prev_nm_size = self.k_nm.shape
+        k_nm_updated = np.zeros((prev_nm_size[0], prev_nm_size[1] + 1))
 
         index = 0
         for train_struc in self.training_structures:
-            struc_kern = self.structure_kernel(sparse_env, train_struc)
+            struc_kern = self.structure_kernel(atomic_env, train_struc)
             kern_len = len(struc_kern)
-            K_nm_updated[index:index+kern_len, prev_nm_size[1]+1] = \
+            k_nm_updated[index:index+kern_len, prev_nm_size[1]+1] = \
                 struc_kern
             index += kern_len
 
-        self.K_nm = K_nm_updated
+        self.k_nm = k_nm_updated
 
     def add_structure(self, structure):
         """Adds a training structure to the GP database.
@@ -63,10 +64,10 @@ class SparseGP:
         # update list of training structures
         self.training_structures.append(structure)
 
-        # update K_nm
-        prev_nm_size = self.K_nm.shape
+        # update k_nm
+        prev_nm_size = self.k_nm.shape
 
         # TODO: add len method to struc to conveniently get the number of
         # training labels corresponding to that structure
 
-        # K_nm_updated = np.zeros((prev_nm_size[0], prev_nm_size[1]))
+        # k_nm_updated = np.zeros((prev_nm_size[0], prev_nm_size[1]))
